38994f6d1b5d7844967b5ca759f1a6249b0dc5d7
Tim Kennedy <t.m.knndy@gmail.com>
2013-11-07 02:37:24 +1100

    made Robot immutable

--- src/main/scala/Robot.scala
+++ src/main/scala/Robot.scala
@@ -1,34 +1,46 @@
 import Direction._
 
-class Robot {
-  private var x: Int = _
-  private var y: Int = _
-  private var dir: Direction = _
+trait Robot {
+  def place(x: Int, y: Int, dir: Direction): Robot
+  def move: Robot
+  def left: Robot
+  def right: Robot
+  def report: String
+}
 
-  def place(x: Int, y: Int, dir: Direction) {
-    this.x = x
-    this.y = y
-    this.dir = dir
+object Robot {
+
+  def apply(x: Int, y: Int, dir: Direction): Option[Robot] = {
+    return Some(new RobotImpl(x, y, dir))
   }
-  
-  def move() {
-    dir match {
-      case North => y -= 1
-      case South => y += 1
-      case East => x += 1
-      case West => x -= 1
+
+  private class RobotImpl(x: Int, y: Int, dir: Direction)
+      extends Robot {
+    override def place(x: Int, y: Int, dir: Direction): Robot = {
+      Robot(x, y, dir).getOrElse(this)
     }
-  }
-  
-  def left() {
-    dir = dir.left
-  }
-  
-  def right() {
-    dir = dir.right
-  }
-  
-  def report(): String = {
-    List(dir, "@", x, ",", y).mkString
+
+    override def move: Robot = {
+      val ret = dir match {
+        case North => Robot(x, y - 1, dir)
+        case South => Robot(x, y + 1, dir)
+        case East  => Robot(x + 1, y, dir)
+        case West  => Robot(x - 1, y, dir)
+      }
+      
+      ret.getOrElse(this)
+    }
+
+    override def left: Robot = {
+      Robot(x, y, dir.left).getOrElse(this)
+    }
+
+    override def right: Robot = {
+      Robot(x, y, dir.right).getOrElse(this)
+    }
+
+    override def report: String = {
+      List(dir, "@", x, ",", y).mkString
+    }
   }
 }
--- src/test/scala/RobotTest.scala
+++ src/test/scala/RobotTest.scala
@@ -5,32 +5,37 @@ import org.specs2.specification._
 object RobotSpec extends Specification {
   "Robot" should {
     trait c1 extends Scope  {
-      val robot = new Robot
+      val robot: Robot = Robot(0, 0, Direction.North).get
     }
     "report correctly after being placed" in new c1 {
-      robot.place(1, 2, Direction.North)
-      robot.report() mustEqual "North@1,2"
+      robot
+      .place(1, 2, Direction.North)
+      .report mustEqual "North@1,2"
     }
     "report correctly after being placed and moved" in new c1 {
-      robot.place(1, 2, Direction.North)
-      robot.move()
-      robot.report() mustEqual "North@1,1"
+      robot
+      .place(1, 2, Direction.North)
+      .move
+      .report mustEqual "North@1,1"
     }
     "report correctly after being placed and rotated" in new c1 {
-      robot.place(1, 3, Direction.North)
-      robot.left()
-      robot.report() mustEqual "West@1,3"
+      robot
+      .place(1, 3, Direction.North)
+      .left
+      .report mustEqual "West@1,3"
     }
     "report correctly after being placed, rotated, and moved" in new c1 {
-      robot.place(1, 3, Direction.East)
-      robot.move()
-      robot.left()
-      robot.report() mustEqual "North@2,3"
+      robot
+      .place(1, 3, Direction.East)
+      .move
+      .left
+      .report mustEqual "North@2,3"
     }
     "report correctly after being placed twice" in new c1 {
-      robot.place(1, 2, Direction.North)
-      robot.place(4, 5, Direction.South)
-      robot.report() mustEqual "South@4,5"
+      robot
+      .place(1, 2, Direction.North)
+      .place(4, 5, Direction.South)
+      .report mustEqual "South@4,5"
     }
   }
 }
